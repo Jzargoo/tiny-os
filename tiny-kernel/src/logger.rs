b9bf87e2a557a4bb84e8905c0d0d367467199974
Jzargo <z.ishanov04@gmail.com>
2026-06-19 16:55:48 +0300

    improved ring buffer to work with bytes. Changed logger to support copying content from buffer to the output String, added loger to support Write trait

--- tiny-kernel/src/logger.rs
+++ tiny-kernel/src/logger.rs
@@ -19,18 +19,59 @@ macro_rules! panic_flush {
     };
 }
 
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => {{
+        $crate::logger::_print(format_args!($($arg)*));
+    }};
+}
+
+#[macro_export]
+macro_rules! println {
+    () => {
+        $crate::print!("\n")
+    };
+    ($($arg:tt)*) => {{
+        $crate::logger::_print(format_args!($($arg)*));
+        $crate::print!("\n");
+    }};
+}
+
+
+#[doc(hidden)]
+pub fn _print(args: core::fmt::Arguments) {
+    use core::fmt::Write;
+    
+    let mut logger = LOGGER.lock(); 
+    
+    let _ = logger.write_fmt(args);
+    
+}
+
 #[doc(hidden)]
 pub fn _panic_flush(){
+    
+    FALLBACK.write_string("Panic flush: ");
 
+    unsafe {
+        LOGGER.force_unlock();
+    };
+    
     let mut log = LOGGER.lock();
-    log.write("Eror occured. Write after panic flush macros");
+
+    
     if log.is_sinks_inited() {
         log.flush_all();
     } else {
+
         let mut line = log.read();
+        
+        FALLBACK.write_string("Panic flush: ");
+
         while line.is_some() {
             FALLBACK.write_string(line.unwrap());
             line = log.read();
         }
     }
 }
+
--- tiny-kernel/src/logger/ring_buffer.rs
+++ tiny-kernel/src/logger/ring_buffer.rs
@@ -1,34 +1,42 @@
+use core::str::from_utf8;
+
 pub struct RingBuffer {
-    buffer: [Option<&'static str>; 50],
+    buffer: [u8; 64 * 1024],
     head: usize,
     tail: usize,
 }
 
-
+#[allow(dead_code)]
 impl RingBuffer {
 
     pub const fn new() -> Self {
         Self {
-            buffer: [None; 50],
+            buffer: [0; 64 * 1024],
          
             head:0,
-         
             tail:0
         }
     }
 
     pub fn push(&mut self, data: &'static str) {
-        
-        self.buffer[self.head] = Some(data);
-
-        self.head = (self.head + 1) % self.buffer.len();
-
-        if self.head == self.tail {
-            self.tail = (self.tail + 1) % self.buffer.len();
+        for byte in data.bytes() {
+            self.push_byte(byte);
         }
     }
 
-    pub fn pop(&mut self) -> Option<&'static str> {
+    pub fn push_byte(&mut self, byte: u8) {
+        
+        self.buffer[self.head] = byte;
+        
+        self.head = (self.head + 1) % self.buffer.len();
+        
+        if self.head == self.tail {
+            self.tail = (self.tail + 1) % self.buffer.len();
+        }
+
+    }
+
+    pub fn pop(&mut self) -> Option<u8> {
 
         if self.tail == self.head {
             return None;
@@ -38,7 +46,39 @@ impl RingBuffer {
 
         self.tail = (self.tail + 1) % self.buffer.len();
 
-        data
+        Some(data)
+    }
+
+    pub fn  popln<'a>(&mut self, buffer: &'a mut [u8]) -> Option<&'a str> {
+
+        if self.head == self.tail {
+            return None;
+        }
+
+        let mut idx = 0;
+
+        while idx < buffer.len() {
+            
+            if let Some(byte) = self.pop() {
+                
+                buffer[idx] = byte;
+                
+                idx += 1;
+
+                if byte == b'\n' {
+                    break;
+                }
+
+            } else {
+                break;
+            }
+        }
+
+        if idx == 0 {
+            return None;
+        }
+
+        from_utf8(&buffer[..idx]).ok()
     }
 
 }
